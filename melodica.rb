3a5d4d5ab6eb26b345e7f00105343864dccdb0a8
zombocoder <root@zombocoder.com>
2024-11-08 18:44:19 +0200

    Code clean up

--- melodica.rb
+++ melodica.rb
@@ -2,7 +2,7 @@ class Melodica < Formula
     desc "Melodica is a console-based audio player built with Go"
     homepage "https://github.com/zombocoder/melodica"
     url "https://github.com/zombocoder/melodica/archive/v0.0.2.tar.gz"
-    sha256 "6f66853918049596774ccfa098109aa57d8b852bbfddfc88e5773346375cdb8c" # Replace with the SHA256 hash of the tarball
+    sha256 "6f66853918049596774ccfa098109aa57d8b852bbfddfc88e5773346375cdb8c"
     license "MIT"
     version "0.0.2"
   
